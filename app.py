0cceeab06ae8283f7fa102b17992ef76635ca03d
Matias Arc <snm.matias@gmail.com>
2024-07-02 23:38:28 -0300

    cambio nombres app.py. Todas las funciones andando

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ class Catalogo:
         self.cursor = self.conn.cursor(dictionary=True)
         
     #----------------------------------------------------------------
-    def agregar_producto(self, nombre, edad, dineroTotal, imagen, nroDeCompras):
+    def agregar_usuario(self, nombre, edad, dineroTotal, imagen, nroDeCompras):
                
         sql = "INSERT INTO usuarios (nombre, edad, dineroTotal, imagen_url, nroDeCompras) VALUES (%s, %s, %s, %s, %s)"
         valores = (nombre, edad, dineroTotal, imagen, nroDeCompras)
@@ -72,47 +72,47 @@ class Catalogo:
         return self.cursor.lastrowid
 
     #----------------------------------------------------------------
-    def consultar_producto(self, idUsuario):
-        # Consultamos un producto a partir de su código
+    def consultar_usuario(self, idUsuario):
+        # Consultamos un usuario a partir de su código
         self.cursor.execute(f"SELECT * FROM usuarios WHERE idUsuario = {idUsuario}")
         return self.cursor.fetchone()
 
     #----------------------------------------------------------------
-    def modificar_producto(self, idUsuario, nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor):
+    def modificar_usuario(self, idUsuario, nuevo_nombre, nueva_edad, nuevo_dineroTotal, nueva_imagen, nuevo_cantidadCompras):
         sql = "UPDATE usuarios SET nombre = %s, edad = %s, dineroTotal = %s, imagen_url = %s, nroDeCompras = %s WHERE idUsuario = %s"
-        valores = (nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor, idUsuario)
+        valores = (nuevo_nombre, nueva_edad, nuevo_dineroTotal, nueva_imagen, nuevo_cantidadCompras, idUsuario)
         self.cursor.execute(sql, valores)
         self.conn.commit()
         return self.cursor.rowcount > 0
 
     #----------------------------------------------------------------
-    def listar_productos(self):
+    def listar_usuarios(self):
         self.cursor.execute("SELECT * FROM usuarios")
         usuarios = self.cursor.fetchall()
         return usuarios
 
     #----------------------------------------------------------------
-    def eliminar_producto(self, idUsuario):
-        # Eliminamos un producto de la tabla a partir de su código
+    def eliminar_usuarios(self, idUsuario):
+        # Eliminamos un usuario de la tabla a partir de su código
         self.cursor.execute(f"DELETE FROM usuarios WHERE idUsuario = {idUsuario}")
         self.conn.commit()
         return self.cursor.rowcount > 0
 
     #----------------------------------------------------------------
-    def mostrar_producto(self, idUsuario):
-        # Mostramos los datos de un producto a partir de su código
-        producto = self.consultar_producto(idUsuario)
-        if producto:
+    def mostrar_usuarios(self, idUsuario):
+        # Mostramos los datos de un usuario a partir de su código
+        usuario = self.consultar_usuario(idUsuario)
+        if usuario:
             print("-" * 40)
-            print(f"ID Usuario.: {producto['idUsuario']}")
-            print(f"Nombre.....: {producto['nombre']}")
-            print(f"Edad.......: {producto['edad']}")
-            print(f"Dinero.....: {producto['dineroTotal']}")
-            print(f"Imagen.....: {producto['imagen_url']}")
-            print(f"Proveedor..: {producto['nroDeCompras']}")
+            print(f"ID Usuario.: {usuario['idUsuario']}")
+            print(f"Nombre.....: {usuario['nombre']}")
+            print(f"Edad.......: {usuario['edad']}")
+            print(f"Dinero.....: {usuario['dineroTotal']}")
+            print(f"Imagen.....: {usuario['imagen_url']}")
+            print(f"Proveedor..: {usuario['nroDeCompras']}")
             print("-" * 40)
         else:
-            print("Producto no encontrado.")
+            print("Usuario no encontrado.")
 
 
 #--------------------------------------------------------------------
@@ -125,7 +125,7 @@ catalogo = Catalogo(host='sql10.freemysqlhosting.net', user='sql10717353', passw
 
 
 # Carpeta para guardar las imagenes.
-RUTA_DESTINO = 'static/imagenes/'
+RUTA_DESTINO = 'imagenesUsr/'
 
 #Al subir al servidor, deberá utilizarse la siguiente ruta. USUARIO debe ser reemplazado por el nombre de usuario de Pythonanywhere
 #RUTA_DESTINO = '/home/USUARIO/mysite/static/imagenes'
@@ -137,32 +137,32 @@ RUTA_DESTINO = 'static/imagenes/'
 #La ruta Flask /usuarios con el método HTTP GET está diseñada para proporcionar los detalles de todos los usuarios almacenados en la base de datos.
 #El método devuelve una lista con todos los usuarios en formato JSON.
 @app.route("/usuarios", methods=["GET"])
-def listar_productos():
-    usuarios = catalogo.listar_productos()
+def listar_usuarios():
+    usuarios = catalogo.listar_usuarios()
     return jsonify(usuarios)
 
 
 #--------------------------------------------------------------------
-# Mostrar un sólo producto según su código
+# Mostrar un sólo usuario según su código
 #--------------------------------------------------------------------
-#La ruta Flask /usuarios/<int:idUsuario> con el método HTTP GET está diseñada para proporcionar los detalles de un producto específico basado en su código.
-#El método busca en la base de datos el producto con el código especificado y devuelve un JSON con los detalles del producto si lo encuentra, o None si no lo encuentra.
+#La ruta Flask /usuarios/<int:idUsuario> con el método HTTP GET está diseñada para proporcionar los detalles de un usuario específico basado en su código.
+#El método busca en la base de datos el usuario con el código especificado y devuelve un JSON con los detalles del usuario si lo encuentra, o None si no lo encuentra.
 @app.route("/usuarios/<int:idUsuario>", methods=["GET"])
-def mostrar_producto(idUsuario):
-    producto = catalogo.consultar_producto(idUsuario)
-    if producto:
-        return jsonify(producto), 201
+def mostrar_usuarios(idUsuario):
+    usuario = catalogo.consultar_usuario(idUsuario)
+    if usuario:
+        return jsonify(usuario), 201
     else:
-        return "Producto no encontrado", 404
+        return "Usuario no encontrado", 404
 
 
 #--------------------------------------------------------------------
-# Agregar un producto
+# Agregar un usuario
 #--------------------------------------------------------------------
 @app.route("/usuarios", methods=["POST"])
-#La ruta Flask `/usuarios` con el método HTTP POST está diseñada para permitir la adición de un nuevo producto a la base de datos.
-#La función agregar_producto se asocia con esta URL y es llamada cuando se hace una solicitud POST a /usuarios.
-def agregar_producto():
+#La ruta Flask `/usuarios` con el método HTTP POST está diseñada para permitir la adición de un nuevo usuario a la base de datos.
+#La función agregar_usuario se asocia con esta URL y es llamada cuando se hace una solicitud POST a /usuarios.
+def agregar_usuario():
     #Recojo los datos del form
     nombre = request.form['nombre']
     edad = request.form['edad']
@@ -174,32 +174,32 @@ def agregar_producto():
     
     # Genero el nombre de la imagen.
     nombre_imagen = secure_filename(imagen.filename) #Chequea el nombre del archivo de la imagen, asegurándose de que sea seguro para guardar en el sistema de archivos
-    #nombre_base, extension = os.path.splitext(nombre_imagen) #Separa el nombre del archivo de su extensión.
-    #nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}" #Genera un nuevo nombre para la imagen usando un timestamp, para evitar sobreescrituras y conflictos de nombres.
+    nombre_base, extension = os.path.splitext(nombre_imagen) #Separa el nombre del archivo de su extensión.
+    nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}" #Genera un nuevo nombre para la imagen usando un timestamp, para evitar sobreescrituras y conflictos de nombres.
 
-    nuevo_codigo = catalogo.agregar_producto(nombre, edad, dineroTotal, nombre_imagen, nroDeCompras)
+    nuevo_codigo = catalogo.agregar_usuario(nombre, edad, dineroTotal, nombre_imagen, nroDeCompras)
     if nuevo_codigo:    
         imagen.save(os.path.join(RUTA_DESTINO, nombre_imagen))
         print("Se guardo!")
-        #Si el producto se agrega con éxito, se devuelve una respuesta JSON con un mensaje de éxito y un código de estado HTTP 201 (Creado).
-        return jsonify({"mensaje": "Producto agregado correctamente.", "idUsuario": nuevo_codigo, "imagen": nombre_imagen}), 201
+        #Si el usuario se agrega con éxito, se devuelve una respuesta JSON con un mensaje de éxito y un código de estado HTTP 201 (Creado).
+        return jsonify({"mensaje": "Usuario agregado correctamente.", "idUsuario": nuevo_codigo, "imagen": nombre_imagen}), 201
     else:
-        #Si el producto no se puede agregar, se devuelve una respuesta JSON con un mensaje de error y un código de estado HTTP 500 (Internal Server Error).
-        return jsonify({"mensaje": "Error al agregar el producto pyth."}), 500
+        #Si el usuario no se puede agregar, se devuelve una respuesta JSON con un mensaje de error y un código de estado HTTP 500 (Internal Server Error).
+        return jsonify({"mensaje": "Error al agregar el usuario pyth."}), 500
     
 
 #--------------------------------------------------------------------
-# Modificar un producto según su código
+# Modificar un usuario según su código
 #--------------------------------------------------------------------
 @app.route("/usuarios/<int:idUsuario>", methods=["PUT"])
-#La ruta Flask /usuarios/<int:idUsuario> con el método HTTP PUT está diseñada para actualizar la información de un producto existente en la base de datos, identificado por su código.
-#La función modificar_producto se asocia con esta URL y es invocada cuando se realiza una solicitud PUT a /usuarios/ seguido de un número (el código del producto).
-def modificar_producto(idUsuario):
+#La ruta Flask /usuarios/<int:idUsuario> con el método HTTP PUT está diseñada para actualizar la información de un usuario existente en la base de datos, identificado por su código.
+#La función modificar_usuario se asocia con esta URL y es invocada cuando se realiza una solicitud PUT a /usuarios/ seguido de un número (el código del usuario).
+def modificar_usuario(idUsuario):
     #Se recuperan los nuevos datos del formulario
-    nueva_descripcion = request.form.get("nombre")
-    nueva_cantidad = request.form.get("edad")
-    nuevo_precio = request.form.get("dineroTotal")
-    nuevo_proveedor = request.form.get("nroDeCompras")
+    nuevo_nombre = request.form.get("nombre")
+    nueva_edad = request.form.get("edad")
+    nuevo_dineroTotal = request.form.get("dineroTotal")
+    nuevo_cantidadCompras = request.form.get("nroDeCompras")
     
     
     # Verifica si se proporcionó una nueva imagen
@@ -213,10 +213,10 @@ def modificar_producto(idUsuario):
         # Guardar la imagen en el servidor
         imagen.save(os.path.join(RUTA_DESTINO, nombre_imagen))
         
-        # Busco el producto guardado
-        producto = catalogo.consultar_producto(idUsuario)
-        if producto: # Si existe el producto...
-            imagen_vieja = producto["imagen_url"]
+        # Busco el usuario guardado
+        usuario = catalogo.consultar_usuario(idUsuario)
+        if usuario: # Si existe el usuario...
+            imagen_vieja = usuario["imagen_url"]
             # Armo la ruta a la imagen
             ruta_imagen = os.path.join(RUTA_DESTINO, imagen_vieja)
 
@@ -225,34 +225,34 @@ def modificar_producto(idUsuario):
                 os.remove(ruta_imagen)
     
     else:
-        # Si no se proporciona una nueva imagen, simplemente usa la imagen existente del producto
-        producto = catalogo.consultar_producto(idUsuario)
-        if producto:
-            nombre_imagen = producto["imagen_url"]
+        # Si no se proporciona una nueva imagen, simplemente usa la imagen existente del usuario
+        usuario = catalogo.consultar_usuario(idUsuario)
+        if usuario:
+            nombre_imagen = usuario["imagen_url"]
 
 
-    # Se llama al método modificar_producto pasando el idUsuario del producto y los nuevos datos.
-    if catalogo.modificar_producto(idUsuario, nueva_descripcion, nueva_cantidad, nuevo_precio, nombre_imagen, nuevo_proveedor):
+    # Se llama al método modificar_usuario pasando el idUsuario del usuario y los nuevos datos.
+    if catalogo.modificar_usuario(idUsuario, nuevo_nombre, nueva_edad, nuevo_dineroTotal, nombre_imagen, nuevo_cantidadCompras):
         
         #Si la actualización es exitosa, se devuelve una respuesta JSON con un mensaje de éxito y un código de estado HTTP 200 (OK).
-        return jsonify({"mensaje": "Producto modificado"}), 200
+        return jsonify({"mensaje": "Usuario modificado"}), 200
     else:
-        #Si el producto no se encuentra (por ejemplo, si no hay ningún producto con el código dado), se devuelve un mensaje de error con un código de estado HTTP 404 (No Encontrado).
-        return jsonify({"mensaje": "Producto no encontrado"}), 404
+        #Si el usuario no se encuentra (por ejemplo, si no hay ningún usuario con el código dado), se devuelve un mensaje de error con un código de estado HTTP 404 (No Encontrado).
+        return jsonify({"mensaje": "Usuario no encontrado"}), 404
 
 
 
 #--------------------------------------------------------------------
-# Eliminar un producto según su código
+# Eliminar un usuario según su código
 #--------------------------------------------------------------------
 @app.route("/usuarios/<int:idUsuario>", methods=["DELETE"])
-#La ruta Flask /usuarios/<int:idUsuario> con el método HTTP DELETE está diseñada para eliminar un producto específico de la base de datos, utilizando su código como identificador.
-#La función eliminar_producto se asocia con esta URL y es llamada cuando se realiza una solicitud DELETE a /usuarios/ seguido de un número (el código del producto).
-def eliminar_producto(idUsuario):
-    # Busco el producto en la base de datos
-    producto = catalogo.consultar_producto(idUsuario)
-    if producto: # Si el producto existe, verifica si hay una imagen asociada en el servidor.
-        imagen_vieja = producto["imagen_url"]
+#La ruta Flask /usuarios/<int:idUsuario> con el método HTTP DELETE está diseñada para eliminar un usuario específico de la base de datos, utilizando su código como identificador.
+#La función eliminar_usuarios se asocia con esta URL y es llamada cuando se realiza una solicitud DELETE a /usuarios/ seguido de un número (el código del usuario).
+def eliminar_usuarios(idUsuario):
+    # Busco el usuario en la base de datos
+    usuario = catalogo.consultar_usuario(idUsuario)
+    if usuario: # Si el usuario existe, verifica si hay una imagen asociada en el servidor.
+        imagen_vieja = usuario["imagen_url"]
         # Armo la ruta a la imagen
         ruta_imagen = os.path.join(RUTA_DESTINO, imagen_vieja)
 
@@ -260,16 +260,16 @@ def eliminar_producto(idUsuario):
         if os.path.exists(ruta_imagen):
             os.remove(ruta_imagen)
 
-        # Luego, elimina el producto del catálogo
-        if catalogo.eliminar_producto(idUsuario):
-            #Si el producto se elimina correctamente, se devuelve una respuesta JSON con un mensaje de éxito y un código de estado HTTP 200 (OK).
-            return jsonify({"mensaje": "Producto eliminado"}), 200
+        # Luego, elimina el usuario del catálogo
+        if catalogo.eliminar_usuarios(idUsuario):
+            #Si el usuario se elimina correctamente, se devuelve una respuesta JSON con un mensaje de éxito y un código de estado HTTP 200 (OK).
+            return jsonify({"mensaje": "Usuario eliminado"}), 200
         else:
-            #Si ocurre un error durante la eliminación (por ejemplo, si el producto no se puede eliminar de la base de datos por alguna razón), se devuelve un mensaje de error con un código de estado HTTP 500 (Error Interno del Servidor).
-            return jsonify({"mensaje": "Error al eliminar el producto"}), 500
+            #Si ocurre un error durante la eliminación (por ejemplo, si el usuario no se puede eliminar de la base de datos por alguna razón), se devuelve un mensaje de error con un código de estado HTTP 500 (Error Interno del Servidor).
+            return jsonify({"mensaje": "Error al eliminar el usuario"}), 500
     else:
-        #Si el producto no se encuentra (por ejemplo, si no existe un producto con el idUsuario proporcionado), se devuelve un mensaje de error con un código de estado HTTP 404 (No Encontrado). 
-        return jsonify({"mensaje": "Producto no encontrado"}), 404
+        #Si el usuario no se encuentra (por ejemplo, si no existe un usuario con el idUsuario proporcionado), se devuelve un mensaje de error con un código de estado HTTP 404 (No Encontrado). 
+        return jsonify({"mensaje": "Usuario no encontrado"}), 404
 
 #--------------------------------------------------------------------
 if __name__ == "__main__":
